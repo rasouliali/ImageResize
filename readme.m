183002756b8bf5afb573dfb202f290641e47aeb0
ali rasouli <ali.r.programmer@gmail.com>
2023-05-26 19:59:03 +0330

    Update readme.m

--- readme.m
+++ readme.m
@@ -4,7 +4,7 @@ This project is used from SkiaSharp library and SkiaSharp.NativeAssets.Linux lib
 
 
 
-<pre>
+`
         public IActionResult Index()
         {
             return View();
@@ -27,20 +27,7 @@ This project is used from SkiaSharp library and SkiaSharp.NativeAssets.Linux lib
             fileContents.Seek(0, SeekOrigin.Begin);
             using SKBitmap sourceBitmap = SKBitmap.Decode(fileContents);
 
-            maxWidth = maxWidth == 0 ? sourceBitmap.Width : maxWidth;
-            maxWidth = maxWidth > 600 ? 600 : maxWidth;
-            maxHeight = maxHeight == 0 ? sourceBitmap.Height : maxHeight;
-            maxHeight = maxHeight > 600 ? 600 : maxHeight;
-            if (Math.Abs((sourceBitmap.Width * 1.0 / maxWidth) - (sourceBitmap.Height * 1.0 / maxHeight)) > 0.1)
-            {
-                var w = (sourceBitmap.Width * 1.0 / maxWidth);
-                var newHeight = (sourceBitmap.Height * 1.0 / w);
-                maxHeight = (int)newHeight;
-            }
-            int height = Math.Min(maxHeight, sourceBitmap.Height);
-            int width = Math.Min(maxWidth, sourceBitmap.Width);
-
-            using SKBitmap scaledBitmap = sourceBitmap.Resize(new SKImageInfo(width, height), quality);
+            using SKBitmap scaledBitmap = sourceBitmap.Resize(new SKImageInfo(maxWidth, maxHeight), quality);
             using SKImage scaledImage = SKImage.FromBitmap(scaledBitmap);
             using SKData data = scaledImage.Encode();
             using (var fileStream = System.IO.File.Create(filePath))
@@ -50,4 +37,4 @@ This project is used from SkiaSharp library and SkiaSharp.NativeAssets.Linux lib
                 myStream.CopyTo(fileStream);
             }
         }
-</pre>
+`
