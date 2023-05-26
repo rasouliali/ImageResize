f0a36c99e3df0ec25598444ef62ec98c3b888b7c
ali rasouli <ali.r.programmer@gmail.com>
2023-05-26 19:47:19 +0330

    Update readme.m

--- readme.m
+++ readme.m
@@ -1,3 +1,3 @@
 This project is Image Resizer for Linux machins.
   
- This project is used from SkiaSharp library and SkiaSharp.NativeAssets.Linux library.
+This project is used from SkiaSharp library and SkiaSharp.NativeAssets.Linux library.
